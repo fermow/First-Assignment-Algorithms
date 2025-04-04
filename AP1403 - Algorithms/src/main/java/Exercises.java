9533181e6b5bafd0f49dffd01320cbe764f34ef4
Mobin Abedian <fermow.fermow85@gmail.com>
2025-04-04 17:13:40 +0330

    new files

--- AP1403 - Algorithms/src/main/java/Exercises.java
+++ AP1403 - Algorithms/src/main/java/Exercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercises {
 
     /*
@@ -8,12 +10,22 @@ public class Exercises {
 
         note: you should return the indices in ascending order and every array's solution is unique
     */
-    public int[] productIndices(int[] values, int target) {
-        // todo
+    public static int[] productIndices(int[] values, int target) {
+        for(int i = 0 ; i < values.length ; i++)
+        {
+            for(int j = i+1 ; j < values.length ; j++)
+            {
+                if(values[i]*values[j] == target )
+                    return new int[]{i,j} ;
+            }
+
+        }
+
         return null;
     }
-
     /*
+
+
         given a matrix of random integers, you should do spiral traversal in it
         e.g. if the matrix is as shown below:
         1 2 3
@@ -23,11 +35,56 @@ public class Exercises {
         {1, 2, 3, 6, 9, 8, 7, 4, 5}
 
         so you should walk in that matrix in a curl and then add the numbers in order you've seen them in a 1D array
-    */
-    public int[] spiralTraversal(int[][] values, int rows, int cols) {
-        // todo
-        return null;
+        */
+
+
+    public static int[] spiralTraversal(int[][] values, int rows, int cols) {
+
+        int[] result = new int[rows*cols];
+        int index = 0;
+
+        int top = 0, bottom = rows - 1;
+        int left = 0, right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                result[index++] = values[top][i];
+            }
+            top++;
+
+
+            for (int i = top; i <= bottom; i++) {
+                result[index++] = values[i][right];
+            }
+            right--;
+
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--) {
+                    result[index++] = values[bottom][i];
+                }
+                bottom--;
+            }
+
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--) {
+                    result[index++] = values[i][left];
+                }
+                left++;
+            }
+
+        }
+        return result;
+
+
+
+
+
     }
+
+
 
     /*
         integer partitioning is a combinatorics problem in discreet maths
@@ -50,15 +107,41 @@ public class Exercises {
 
         hint: you can measure the size and order of arrays by finding the pattern of partitions and their number
         trust me, that one's fun and easy :)
+*/
 
-        if you're familiar with lists and arraylists, you can also edit method's body to use them instead of array
-    */
     public int[][] intPartitions(int n) {
-        // todo
-        return null;
+        int[][] result = new int[100][n];
+        int[] partition = new int[n];
+        int[] index = new int[1];
+
+        partitionHelper(n, partition, index, result, n, 0);
+
+        int[][] resizedResult = new int[index[0]][];
+        for (int i = 0; i < index[0]; i++) {
+            resizedResult[i] = new int[result[i].length];
+            System.arraycopy(result[i], 0, resizedResult[i], 0, result[i].length);
+        }
+        return resizedResult;
     }
 
+    private void partitionHelper(int n, int[] partition, int[] index, int[][] result, int max, int currentSize) {
+        if (n == 0) {
+            int[] newPartition = new int[currentSize];
+            System.arraycopy(partition, 0, newPartition, 0, currentSize);
+            result[index[0]] = newPartition;
+            index[0]++;
+            return;
+        }
+
+        for (int i = Math.min(n, max); i >= 1; i--) {
+            partition[currentSize] = i;
+            partitionHelper(n - i, partition, index, result, i, currentSize + 1);
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        // you can test your code here
+        System.out.println("hi");
     }
 }
